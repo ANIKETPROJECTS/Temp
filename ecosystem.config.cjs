5654b205f644d420e2f9bb6854ab63a484607d34
ANIKETPROJECTS <aniketraneworkemail@gmail.com>
2026-02-13 14:50:50 +0000

    ns

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,8 +6,8 @@ module.exports = {
       env: {
         NODE_ENV: "production",
         PORT: 3004,
-        MONGODB_URI: "your_mongodb_uri_here",
-        SESSION_SECRET: "your_session_secret_here",
+        MONGODB_URI: "mongodb+srv://mrunaln304_db_user:5aAJKpma893SBqRg@queuecafe.vzczmuh.mongodb.net/shubhamcafe?appName=queuecafe",
+        SESSION_SECRET: "GqzNSR4MUH530nWSx315yio6te8okKeQgmKfpXydiqHNPHnt/3fc7xT/G4Z1HshVbk2ntD8g5wZ0DVBiBBvbjg==",
         TWILIO_ACCOUNT_SID: "your_twilio_sid_here",
         TWILIO_AUTH_TOKEN: "your_twilio_token_here",
         TWILIO_PHONE_NUMBER: "your_twilio_phone_number_here",
